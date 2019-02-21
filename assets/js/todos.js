8c9917f996cfaba8c4cbb8f7e839cc0db1c80d35
Diana Wallace <dianawallacedeveloper@gmail.com>
2019-02-21 13:41:39 +0000

    added check off todos function

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,4 +1,12 @@
 
+// check off specific todos by clicking
+$("ul").on("click", "li", function() {
+    $(this).toggleClass("completed");
+  });
+  
+
+
+
 
 $(".fa-plus").click(function() {
     $("input[type='text']").fadeToggle();
