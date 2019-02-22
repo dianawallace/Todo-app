afe830463eaf5b4163e834105fd8a7d61bb136a2
Diana Wallace <dianawallacedeveloper@gmail.com>
2019-02-22 14:00:22 +0000

    added click on trashcan to delete todo function

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -4,7 +4,15 @@ $("ul").on("click", "li", function() {
     $(this).toggleClass("completed");
   });
   
-
+  // Click on trashcan to delete todo
+  $("ul").on("click", "span", function(event) {
+    $(this)
+      .parent()
+      .fadeOut(500, function() {
+        $(this).remove();
+      });
+    event.stopPropagation();
+  });
 
 
 
